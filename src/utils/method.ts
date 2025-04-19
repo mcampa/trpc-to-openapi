7dd00d45e71c76acd7762196c8b916ab64bf911a
Mario Campa <mcampa@gmail.com>
2025-04-19 15:03:02 -0700

    Upgrade to @trpc/server 11.1.0

--- src/utils/method.ts
+++ src/utils/method.ts
@@ -1,8 +1,16 @@
-import { OpenApiMethod } from '../types';
+import { NodeHTTPRequest, OpenApiMethod } from '../types';
 
 export const acceptsRequestBody = (method: OpenApiMethod | 'HEAD') => {
   if (method === 'GET' || method === 'DELETE') {
     return false;
   }
   return true;
+};
+
+export const getContentType = (req: NodeHTTPRequest | Request): string | undefined => {
+  if (req instanceof Request) {
+    return req.headers.get('content-type') ?? undefined;
+  }
+
+  return req.headers['content-type'] ?? undefined;
 };
